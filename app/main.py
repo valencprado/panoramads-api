7655d7fbbaa60b1759d6cb46e6e7db75c84d7ff5
Valentina Prado <valenpschool@gmail.com>
2025-01-15 22:50:42 -0300

    retorno estado inicial

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from .service import get_structured_data
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 app = FastAPI()
 
