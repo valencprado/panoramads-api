0af2637dddd8f0bf89f5dfbef1a6cdccfd8d07a3
Valentina Prado <valenpschool@gmail.com>
2025-01-15 22:42:31 -0300

    teste

--- app/service.py
+++ app/service.py
@@ -1,19 +1,19 @@
 import gspread
 import os
 
-credentials = {
-  "type": "service_account",
-  "project_id": os.getenv("PROJECT_ID"),
-  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-  "private_key": os.getenv("PRIVATE_KEY"),
-  "client_email": os.getenv("CLIENT_EMAIL"),
-  "client_id": os.getenv("CLIENT_ID"),
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": os.getenv("CLIENT_URL"),
-  "universe_domain": "googleapis.com"
-}
+# credentials = {
+#   "type": "service_account",
+#   "project_id": os.getenv("PROJECT_ID"),
+#   "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+#   "private_key": os.getenv("PRIVATE_KEY"),
+#   "client_email": os.getenv("CLIENT_EMAIL"),
+#   "client_id": os.getenv("CLIENT_ID"),
+#   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#   "token_uri": "https://oauth2.googleapis.com/token",
+#   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#   "client_x509_cert_url": os.getenv("CLIENT_URL"),
+#   "universe_domain": "googleapis.com"
+# }
 
 def count_values(arr):
     return [{"value": value, "count": arr.count(value)} for value in set(arr)]
