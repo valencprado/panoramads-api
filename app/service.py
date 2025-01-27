f9c2e5560a3781148c11df2067cfbf8a802a0d7a
Valentina Prado <valenpschool@gmail.com>
2025-01-27 13:23:03 -0300

    fix: api key

--- app/service.py
+++ app/service.py
@@ -9,7 +9,7 @@ credentials = {
   "type": "service_account",
   "project_id": os.getenv("PROJECT_ID"),
   "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-  "private_key": os.getenv("PRIVATE_KEY"),
+  "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
   "client_email": os.getenv("CLIENT_EMAIL"),
   "client_id": os.getenv("CLIENT_ID"),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
